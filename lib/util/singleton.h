b3c1e96d526db924f2503f6c67cf639251d7c3fb
xiaguan <751080330@qq.com>
2022-03-12 19:35:22 +0800

    repair log and add util folder

---
+++ lib/util/singleton.h
@@ -0,0 +1,30 @@
+﻿//
+// Created by 75108 on 2022/3/12.
+//
+
+#ifndef LIB_TEST_SINGLETON_H
+#define LIB_TEST_SINGLETON_H
+
+#include <memory>
+
+namespace su{
+    template <typename T,class X = void,int N = 0>
+    class Singleton{
+    public:
+        static T * GetInstance(){
+            static T v;
+            return &v;
+        }
+    };
+
+    template <typename T,class X = void,int N = 0>
+    class SingletonPtr{
+    public:
+        static std::shared_ptr<T> GetInstance(){
+            static std::shared_ptr<T> v(new T);
+            return v;
+        }
+    };
+}
+
+#endif //LIB_TEST_SINGLETON_H
